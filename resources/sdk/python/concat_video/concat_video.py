48228beb3233a2c9b2ec20d90e4b531449b3ad7f
weisiwu <siwu.wsw@autonavi.com>
2024-03-28 20:47:16 +0800

    feat: 决定用python做视频合并

--- resources/sdk/python/concat_video/concat_video.py
+++ resources/sdk/python/concat_video/concat_video.py
@@ -4,6 +4,7 @@ import sys
 import json
 import glob
 import argparse
+import numpy as np
 from pathlib import *
 from PIL import Image
 import moviepy.editor as mp
@@ -26,7 +27,6 @@ class CacheConfig:
     """
     图片处理过程，使用到的内部设置，如切割完的视频片段存放位置、关键帧存放位置等
     """
-
     def __init__(self) -> None:
         # 视频片段缓存
         self.video_parts_cahce_path = Path.cwd() / "video_parts_cahce"
@@ -37,74 +37,60 @@ class CacheConfig:
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default="")
+    parser.add_argument("--image_path", type=str, default="")
+    parser.add_argument("--background_music_path", type=str, default="")
     parser.add_argument("--config_file", type=str, default="")
     return parser.parse_args()
 
 
-def concat_imgs_to_video(raw_data, selected_imgs_str=""):
-    data = json.loads(raw_data)
+def concat_imgs_to_video(image_path, background_music_path):
+    image_path = Path(image_path)
+    images = os.listdir(image_path)
+    imgs_num = len(images)
+    img_size = None
+    transition_duration_rate = 0.15
 
-    if not len(data):
+    if not imgs_num:
         print(json.dumps({"code": 0, "type": "concat_video", "message": "no picture"}))
         sys.stdout.flush()
         return False
 
+    for image in images:
+        print("都试试什么图片", image)
+        if not img_size:
+            img_size = Image.open(image_path / image).size
+
     # 获取图片尺寸
-    img_size = Image.open(data[0].get("image")).size
     print("图片尺寸是什么", img_size)
     frame_rate = 30
-    return
-    cache_config = CacheConfig()
     outputPath = Path(sd_config["outputPath"])
     tmpSavePath = outputPath / "tmp_save.mp4"
     outputFile = outputPath / sd_config["saveFileName"]
-    # videowrite = cv2.VideoWriter(
-    #     tmpSavePath.as_posix(),
-    #     cv2.VideoWriter_fourcc(*"mp4v"),
-    #     frame_rate,
-    #     img_size,
-    # )
-    frame_img_path = cache_config.video_frames_cahce_path
-    filtered_files = glob.glob(f"{frame_img_path}/*_new*")
+    videowrite = cv2.VideoWriter(
+        tmpSavePath.as_posix(),
+        cv2.VideoWriter_fourcc(*"mp4v"),
+        frame_rate,
+        img_size,
+    )
+    filtered_files = glob.glob(f"{image_path}/*")
     sort_imgs = sorted(filtered_files, key=custom_sort)
 
     for index, frame_img in enumerate(sort_imgs):
-        next_index = index + 1 if index + 1 < imgs_num else None
         img = cv2.resize(cv2.imread(frame_img), img_size)
-        next_img = (
-            cv2.resize(cv2.imread(sort_imgs[next_index]), img_size)
-            if next_index is not None
-            else None
-        )
 
         if img is None:
             continue
 
-        img_duration = (custom_sort(frame_img) - prev_end) / frame_rate
-        total_num = int(frame_rate * img_duration)
-        transition_num = int(total_num * transition_duration_rate) or 1
-        transition_start_index = int(total_num * (1 - transition_duration_rate))
+        total_num = int(frame_rate * 1)
         for cur_index in range(total_num):
-            # 计算过渡效果的每一帧
-            if next_img is not None and cur_index >= transition_start_index:
-                # 计算过渡权重（0到1之间）
-                alpha = (cur_index - transition_start_index) / transition_num
-                # 使用alpha权重进行混合
-                blended_img = cv2.addWeighted(img, 1 - alpha, next_img, alpha, 0)
-                # 将混合后的图像写入视频文件
-                videowrite.write(np.uint8(blended_img))
-            else:
-                videowrite.write(img)
-
-        prev_end = custom_sort(frame_img)
+            videowrite.write(img)
 
     videowrite.release()
     video_file = mp.VideoFileClip(tmpSavePath.as_posix())
     v_duration = video_file.duration
     silent_audio_clip = AudioClip(lambda t: [0, 0], duration=v_duration, fps=44100)
     # 如果视频无音轨，则创建静音音轨
-    audio_file = mp.VideoFileClip(str(input_path)).audio or silent_audio_clip
+    audio_file = mp.VideoFileClip(str(background_music_path)).audio or silent_audio_clip
     a_duration = audio_file.duration
     if v_duration > a_duration:
         video_file = video_file.subclip(0, a_duration)
@@ -129,7 +115,7 @@ def concat_imgs_to_video(raw_data, selected_imgs_str=""):
             {
                 "code": 1,
                 "type": "concat_video",
-                "outputPath": outputPath.as_posix(),
+                "message": "no picture",
                 "outputFile": outputFile.as_posix(),
             }
         )
@@ -143,4 +129,6 @@ with open(args.config_file, "r") as f:
     sd_config = json.load(f)
     sd_config["sdBaseUrl"] = sd_config["sdBaseUrl"].rstrip("/") or ""
 
-concat_imgs_to_video(data=args.data)
+concat_imgs_to_video(
+    image_path=args.image_path, background_music_path=args.background_music_path
+)
