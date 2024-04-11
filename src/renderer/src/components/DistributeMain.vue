449614f2b929163e78c556781c1012e365f65283
weisiwu <siwu.wsw@autonavi.com>
2024-04-11 20:31:18 +0800

    feat: 保存弹窗

--- src/renderer/src/components/DistributeMain.vue
+++ src/renderer/src/components/DistributeMain.vue
@@ -1,43 +1,71 @@
 <template>
   <div class="main">
-    <el-row>
-      <el-col v-if="!selected_file" :span="9"></el-col>
-      <el-col :span="6">
-        <el-button v-if="!selected_file" type="primary" @click="selectFile">选择视频</el-button>
-        <p v-if="selected_file">{{ selected_file.path }}</p>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="9"></el-col>
-      <el-col :span="6">
-        <input v-model="input_val" type="text" placeholder="输入标题" />
-      </el-col>
-      <el-col :span="9"></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="9"></el-col>
-      <el-col :span="6">
-        <input v-model="desc_val" type="text" placeholder="输入描述" />
-      </el-col>
-      <el-col :span="9"></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="4"></el-col>
-      <el-col :span="6"> <el-button type="primary" @click="login">登录</el-button></el-col>
-      <el-col :span="4"></el-col>
-      <el-col :span="6">
-        <el-button type="primary" @click="sendVideo">发视频</el-button>
-      </el-col>
-      <el-col :span="4"></el-col>
-    </el-row>
+    <div :style="{ margin: '15px 20px' }">
+      <el-alert
+        title="【提示】快速分发多稿件到多平台，目前支持B站，单稿件"
+        description="使用前需要您登录授权各个平台，授权后所有平台共用一套模板，一次点击即可完成多平台，多视频分发。"
+        type="success"
+        show-icon
+      />
+    </div>
+    <el-upload class="upload-demo" drag action="" multiple limit="10">
+      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+      <div class="el-upload__text">将待分发视频拖放到这里或者<em>点击选择</em></div>
+      <template #tip>
+        <div class="el-upload__tip">最多可选择10个视频，将按照选中顺序依次进行分发</div>
+      </template>
+      <!-- <el-button v-if="!selected_file" type="primary" @click="selectFile">选择视频</el-button> -->
+    </el-upload>
+    <div :style="{ display: 'flex', justifyContent: 'center', marginTop: '20px' }">
+      <el-button type="primary" @click="login">授权登录</el-button>
+      <div :style="{ width: '30px' }"></div>
+      <el-button type="primary" @click="sendVideo">一键分发</el-button>
+    </div>
+    <!-- 模板 -->
+    <div id="tplBtn">
+      <span @click="drawer = !drawer">查看模板</span>
+    </div>
+    <el-drawer v-model="drawer" direction="btt" :before-close="handleTemplateModelClose">
+      <template #header>
+        <h4>视频信息模板</h4>
+      </template>
+      <template #default> </template>
+      <template #footer>
+        <div style="flex: auto">
+          <el-button @click="handleTemplateModelCancel">取消</el-button>
+          <el-button type="primary" @click="handleTemplateModelConfirm">保存</el-button>
+        </div>
+      </template>
+    </el-drawer>
+    <!-- 分发执行日志 -->
+    <terminal
+      id="terminal"
+      :ref="terminal_ref"
+      :style="{ height: '350px', position: 'fixed', width: '660px', bottom: '30px' }"
+      name="分发执行日志"
+      context="爆肝分发"
+      context-suffix=":"
+      title="分发执行日志"
+      :show-header="false"
+      :auto-help="false"
+      :init-log="{ content: `[${new Date().toLocaleString()}]爆肝分发软件启动~` }"
+      :input-filter="() => ''"
+      @exec-cmd="onExecCmd"
+    ></terminal>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
+import { UploadFilled } from '@element-plus/icons-vue'
+import Terminal from 'vue-web-terminal'
+import 'vue-web-terminal/lib/theme/dark.css'
 
 const input_val = ref('')
 const desc_val = ref('')
+const terminal_ref = ref('')
+const drawer = ref(false)
 const selected_file = ref(null)
 const login = () => {
   window.ipcRenderer.send('platform-login', { platform: 'bilibili' })
@@ -48,6 +76,27 @@ const selectFile = () => {
     selected_file.value = { path, size }
   })
 }
+const onExecCmd = (key, command, success, failed) => {
+  if (key === 'fail') {
+    failed('Something wrong!!!')
+  } else {
+    let allClass = ['success', 'error', 'system', 'info', 'warning']
+
+    let clazz = allClass[Math.floor(Math.random() * allClass.length)]
+    success({
+      type: 'normal',
+      class: clazz,
+      tag: '成功',
+      content: command
+    })
+  }
+}
+
+setTimeout(() => {
+  console.log('wswTest: terminal_ref.value.pushMessage', terminal_ref.value.pushMessage)
+  terminal_ref.value.pushMessage('hello world!')
+}, 100)
+
 const sendVideo = () => {
   window.ipcRenderer.send('platform-send-video', {
     platform: 'bilibili',
@@ -59,6 +108,51 @@ const sendVideo = () => {
     }
   })
 }
+const handleTemplateModelClose = (done) => {
+  ElMessageBox.confirm('是否离开编辑模板?', '注意', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消'
+  })
+    .then(() => done())
+    .catch(() => {})
+}
+const handleTemplateModelCancel = () => {
+  drawer.value = false
+}
+// 保存模板更新
+const handleTemplateModelConfirm = () => {
+  drawer.value = false
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+#tplBtn {
+  position: fixed;
+  top: 50%;
+  right: 0px;
+  margin-top: -50px;
+  width: 50px;
+  height: 100px;
+  background-color: transparent;
+  z-index: 999;
+  span {
+    display: inline-block;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 10px 19px;
+    border-radius: 5px 0px 0px 5px;
+    background: #409eff;
+    color: rgba(255, 255, 255, 0.8);
+    cursor: pointer;
+  }
+  span:hover {
+    color: #fff;
+  }
+}
+#terminal {
+  height: 350px;
+  position: fixed;
+  width: 660px;
+  bottom: 30px;
+}
+</style>
